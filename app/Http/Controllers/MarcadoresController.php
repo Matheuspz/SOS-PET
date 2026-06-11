a6a1c95f6183ef8e2c30855423d4fec396e46adf
Matheus Pesch Zenere <mpzenere@gmail.com>
2026-06-11 04:19:00 -0300

    Tirei o Nullable de Colunas que não deveriam ser nullable

---
+++ app/Http/Controllers/MarcadoresController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Marcadores;
+use Illuminate\Http\Request;
+
+class MarcadoresController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Marcadores $marcadores)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Marcadores $marcadores)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Marcadores $marcadores)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Marcadores $marcadores)
+    {
+        //
+    }
+}
