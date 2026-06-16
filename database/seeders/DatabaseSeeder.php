12afd593a06049ca1358694ebf647bf081dadf2b
Matheus Pesch Zenere <mpzenere@gmail.com>
2026-06-16 13:32:26 -0300

    Atualização no README.md e remoção do Seeder de Admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,10 +17,10 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Admin',
-            'email' => 'admin@example.com',
-            'password' => bcrypt('admin'),
-        ]);
+//        User::factory()->create([
+//            'name' => 'Admin',
+//            'email' => 'admin@example.com',
+//            'password' => bcrypt('admin'),
+//        ]);
     }
 }
